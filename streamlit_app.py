1883da52a56fc6e778dba4ab402525de473d8dca
Caleb Gammon <calebwgammon@gmail.com>
2022-09-28 19:53:43 -0400

    Update streamlit_app.py
    fixed syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-Streamlit.multiselect("Pick your fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick your fruits: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 
