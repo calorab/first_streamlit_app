1bc544b9d2dc3cd667fe35289c48854bf6e9751c
Caleb Gammon <calebwgammon@gmail.com>
2022-09-28 19:52:22 -0400

    Update streamlit_app.py
    moved multi select picker above dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fruit_list)
 
 Streamlit.multiselect("Pick your fruits: ", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
+
