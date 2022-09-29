ec00563c568dd88664518fcead5a5acdc0c7a9c7
Caleb Gammon <calebwgammon@gmail.com>
2022-09-28 20:00:44 -0400

    Update streamlit_app.py
    added fruits_selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.multiselect("Pick your fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick your fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_selected)
 
 
